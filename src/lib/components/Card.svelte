e9a50a25e6129e9a2c839396c2467b68f8661078
Ashkie <ashkiecharles@gmail.com>
2023-11-06 12:34:07 -0700

    changed the files for the images

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -6,11 +6,11 @@
 <section>
 	<h2 class="text-3xl">Fictional Weapons</h2>
 	<ul>
-		{#each weaponList as { href, name, description}}
+		{#each weaponList as { href, name, description, pic}}
 		<a {href}>
 				<div class="cards">
 						<header><h2>{name}</h2></header>
-				<img src="../images/Lightsaber.png" alt="">
+				<img src={pic} alt="">
 				
 				<p>
 						{description}
